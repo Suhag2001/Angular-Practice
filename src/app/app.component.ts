920bcda07c5384bac1918e7aedb5ccf42365e2c1
= <=>
2023-08-23 18:54:40 +0530

    componet file changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Codder';
+
+  getName(name){
+    alert("Hi My Name is : "+name);
+  }
+// Events
+  getData(data){
+       console.warn(data)
+  }
+  getColor(data){
+      console.warn(data);
+  }
+
+  // get text from input and print
+ 
+  storeData="";
+
+  printData(data){
+    console.warn(data);
+    this.storeData = data;
+  }
+
+  Counter = 0 ;
+  //Counter
+   getCount(val){
+     val=='add' ? this.Counter++ : this.Counter--;
+   }
 }
